5d77eb952d51d1f1d650f6d4a615eac6d1759a7d
zehrackr <cakirzehra312@gmail.com>
2022-06-26 20:23:33 +0300

    first commit

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, -5, 10, 48, 7 };
+        BubbleSort.bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
